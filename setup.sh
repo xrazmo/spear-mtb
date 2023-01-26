95d186b0b3ad27f11e829fcd5e4954d05ccc1497
xrazmo <razavi_sm@yahoo.com>
2023-01-26 10:40:11 +0100

    Add refdb download and catalogues updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+echo "1.Creating a conda environment"
 conda deactivate
 conda remove -y -n spear-mtb --all || :
 conda env create -f ./environment.yml
 source activate spear-mtb
+
+echo "2.Downloading the reference databases for clockwork workflow"
+
+wget -O="assets.tar.gz" https://figshare.com/ndownloader/files/38856204
+tar -xvzf assets.tar.gz
+# rm "assets.tar.gz"
+
+echo "3.Updating the catalogues"
+cd ./assets/catalogues/NC_000962.3
+mv *.csv *.csv.BAK
+wget -O WHO-GARC1.csv https://raw.githubusercontent.com/oxfordmmm/tuberculosis_amr_catalogues/public/catalogues/NC_000962.3/NC_000962.3_WHO-UCN-GTB-PCI-2021.7_v1.0_GARC1_RUS.csv
