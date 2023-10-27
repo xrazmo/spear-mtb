f24e0a2dd4d07cb7dbece54038aac965ddbfc2a6
xrazmo <razavi_sm@yahoo.com>
2023-10-27 16:51:57 +0200

    Separate updating catalogues in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+update_catalogues=false 
+
+while getopts ":u" opt; do
+  case $opt in
+    u)
+      update_catalogues=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
 echo "-Creating a conda environment"
 conda deactivate
@@ -8,14 +22,18 @@ source activate spear-mtb
 
 echo "-Downloading the reference databases for CRyPTIC workflow"
 
-wget -O="assets.tar.gz" https://figshare.com/ndownloader/files/38856204
-tar -xvzf assets.tar.gz
-# rm "assets.tar.gz"
+if [ "$update_catalogues" = false ]; then
+  wget -O="assets.tar.gz" https://figshare.com/ndownloader/files/38856204
+  tar -xvzf assets.tar.gz
+  # rm "assets.tar.gz"
+fi
 
-echo "-Updating the catalogues"
-cd ./assets/catalogues/NC_000962.3
-mv *.csv *.csv.BAK
-wget -O WHO_GARC1_v1.csv https://raw.githubusercontent.com/oxfordmmm/tuberculosis_amr_catalogues/public/catalogues/NC_000962.3/NC_000962.3_WHO-UCN-GTB-PCI-2021.7_v1.0_GARC1_RUS.csv
-wget -O CRyPTIC_GARC1_v1-311.csv https://raw.githubusercontent.com/oxfordmmm/tuberculosis_amr_catalogues/public/catalogues/NC_000962.3/NC_000962.3_CRyPTIC_v1.311_GARC1_RUS.csv
+if [ "$update_catalogues" = true ]; then
+  echo "-Updating the catalogues"
+  cd ./assets/catalogues/NC_000962.3
+  mv *.csv *.csv.BAK
+  wget -O WHO_GARC1_v1.csv https://raw.githubusercontent.com/oxfordmmm/tuberculosis_amr_catalogues/public/catalogues/NC_000962.3/NC_000962.3_WHO-UCN-GTB-PCI-2021.7_v1.0_GARC1_RUS.csv
+  wget -O CRyPTIC_GARC1_v1-311.csv https://raw.githubusercontent.com/oxfordmmm/tuberculosis_amr_catalogues/public/catalogues/NC_000962.3/NC_000962.3_CRyPTIC_v1.311_GARC1_RUS.csv
+fi
 
 echo "--Finished--"
